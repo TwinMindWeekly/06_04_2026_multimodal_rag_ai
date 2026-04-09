b088dd6014a896a6a409907a0f7d42178daf1bd4
ThanhNhanDang <yesthanhnhan16@gmail.com>
2026-04-09 10:52:00 +0700

    fix(frontend): correct import path for i18n locales

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -1,8 +1,8 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-import enTranslations from '../locales/en.json';
-import viTranslations from '../locales/vi.json';
+import enTranslations from './locales/en.json';
+import viTranslations from './locales/vi.json';
 
 i18n
   .use(initReactI18next)
